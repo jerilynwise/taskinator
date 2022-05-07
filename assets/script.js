6d4fb33956e61a8a6d446d6c60bf25b40ff1db20
jerilyn.wise@paddywax.com <jerilyn.wise@paddywax.com>
2022-05-07 13:04:09 -0500

    people add tasks to table with task type

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+//create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+//create div to hold task info and to add to list item
+    var taskInfoE1 = document.createElement("div");
+    taskInfoE1.className = "task-info";
+
+//add HTML content to div
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoE1);
+
+//add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
+
 
 formE1.addEventListener('submit',createTaskHandler);
